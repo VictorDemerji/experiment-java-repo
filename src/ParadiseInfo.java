3d1b3e3c654956ebe265a27ed570d944c436cdcb
VictorDemerji <demerji.v1@gmail.com>
2025-09-22 21:36:46 +0300

    Add Paradise Info class

---
+++ src/ParadiseInfo.java
@@ -0,0 +1,11 @@
+public class ParadiseInfo {
+    public static void main(String[] args) {
+
+        displyInfo();
+
+    }
+    public static void displyInfo() {
+        System.out.println("Paradise Day Spa vrea sa re rasfete.");
+        System.out.println("Noi te vom face sa arati mai bine.");
+    }
+}
---
+++ src/TestInfo.java
@@ -0,0 +1,6 @@
+public class TestInfo {
+    public static void main(String[] args) {
+        System.out.println("Apelarea metodei din alta clasa:");
+    ParadiseInfo.displyInfo();
+    }
+}
