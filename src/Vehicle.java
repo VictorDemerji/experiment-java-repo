70359719aa020db83b13a97f7b1b94c80181878d
VictorDemerji <demerji.v1@gmail.com>
2025-09-22 17:39:10 +0300

    Add changes

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -7,7 +7,7 @@ public class Vehicle {
     //and does not return any value (void)
     void distance (double interval) {
         double value = maxspeed * interval;
-        System.out.println("Va parcurge o distanta egala cu " + value + " km.");
+        System.out.println("will travel a distance equal to " + value + " km.");
     }
     // distance(double interval)
     //Vehicule class
@@ -19,16 +19,18 @@ public class Vehicle {
         masina.maxspeed   = 130;
         masina.burnup     = 30;
 
+        //another example of the class Vehicle
         Vehicle autobuz = new Vehicle();
         autobuz.passegers = 45;
         autobuz.wheels    = 4;
         autobuz.maxspeed  = 100;
         autobuz.burnup    = 25;
 
+        //calculating the route traveled in 0.5 hours
         double time = 0.5;
-        System.out.println("Automobilul cu " + masina.passegers + " pasageri ");
+        System.out.print("The car with " + masina.passegers + " passagers ");
         masina.distance(time);
-        System.out.println("Autobuzul cu " + autobuz.passegers + " pasageri ");
+        System.out.print("The bus with " + autobuz.passegers + " passagers ");
         autobuz.distance(time);
     }
 
